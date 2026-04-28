6af82c3b7d7ca955c6164f112e2e9f5c1b5472d3
Pangya12345 <pholphirulp@gmail.com>
2026-04-29 00:54:01 +0700

    change file

--- Udemy 100 day of code/Day19/Turtle_race.py
+++ Udemy 100 day of code/Day19/Turtle_race.py
@@ -2,32 +2,29 @@ from turtle import Turtle, Screen
 import random as r 
 
 random_colour = ["purple", "blue", "green", "yellow", "orange", "red", "pink"]
-random_position_set_up = [0, 50,  -50, 100, -100,  150, -150]
-    
+random_position_set_up = [0, 50, -50, 100, -100, -150, 150]
+random_movement = r.randint(10, 30)
+
+all_turtle = []
 for i in range(7):
     timmy = Turtle()
+    color = r.choice(random_colour)
+    timmy.color(color)
     timmy.shape("turtle")
-    colour = r.choice(random_colour)
-    timmy.color(colour)
-    random_colour.remove(colour)
     timmy.penup()
-    timmy.backward(950)                 
-    timmy.right(90)
-    random_position = r.choice(random_position_set_up)
-    timmy.forward(random_position)
-    random_position_set_up.remove(random_position)
+    timmy.backward(480)
     timmy.left(90)
+    random_direction = r.choice(random_position_set_up)
+    timmy.forward(random_direction)
+    timmy.right(90)
+    random_colour.remove(color)
+    random_position_set_up.remove(random_direction)
+    all_turtle.append(timmy)
 
 
-
-
-
-
-
-    
 
 screen = Screen()
-screen.setup(width = 2000, height = 1000)
+screen.setup(width = 500, height = 1000)
 user_bet = screen.textinput(title = "Turtle racing Game!", prompt = "Enter a color")
 screen.exitonclick()
 
