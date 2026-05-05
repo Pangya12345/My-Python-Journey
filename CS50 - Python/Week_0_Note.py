6fc9cac3991e6651ea95e8bccf77b00ea63f891e
Pangya12345 <pholphirulp@gmail.com>
2026-05-05 12:02:25 +0700

    change

---
+++ CS50 - Python/Week_0_Note.py
@@ -0,0 +1,27 @@
+# =======================================================
+# CS50P - Function & Variables
+#========================================================
+
+# --- 1. print() ---
+# Definition: Print something on the screen.
+print("Hello World")
+print("Hello", "World") # Seperate by , to print the string
+
+
+
+# --- 2. Variables ---
+# Definition: A containers to store the data or value.
+name = "Pangya"
+Age = 17
+weight = 70.65
+
+
+
+# --- 3. input() ---
+# fDefinition: Stop the program and ask for thje user input
+
+
+
+# --- 4. Data Types & Conversion ---
+# Main type: Str(String), int(Integer), float(FLoating Point), Boolean(True or False).
+
