8f3268cf8aa2121ee89fffdcd4028ed9a1229f62
Pangya12345 <pholphirulp@gmail.com>
2026-05-21 16:15:13 +0700

    creating new life

---
+++ CS50 - Python/Note/week0-functions-variables.py
@@ -0,0 +1,44 @@
+# Claude Link --> https://claude.ai/chat/270c464e-a38f-4318-8f62-63078b0f43a2
+
+# ====================================================================================
+# CS50P - Week 0: Function & Variables
+# ====================================================================================
+
+
+# KEYWORD 
+|- Print     |
+|- Variable  |
+|- F-string  |
+|- Data Type |
+|- Input     |
+|- Funciton  |
+|- Return    |
+
+
+
+# --- 1. print() --- 
+# DEFINITION ---> Print something into the terminal or the screen.
+
+print("Hello World") 
+print("Hello", "World") # ---> Can pass multiple values seperated by ,
+
+
+
+
+
+# --- 2. Variable ---
+# DEFINITION ---> Container for storing data or value.
+
+name = "Pangya" 
+age = 15 
+gpa = 3.8
+# No need to declared type
+
+
+
+
+
+# --- 3. F-string ---
+
+
+
