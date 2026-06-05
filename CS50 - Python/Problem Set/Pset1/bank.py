cfd0c69059927070202316b8f0f9b1f5cdb20f26
Pangya12345 <pholphirulp@gmail.com>
2026-06-05 23:21:52 +0700

    gtgtg

--- CS50 - Python/Problem Set/Pset1/bank.py
+++ CS50 - Python/Problem Set/Pset1/bank.py
@@ -10,10 +10,6 @@ if test == "Hello" or test == "hello":
 elif test[0] == "h" or test[0] == "H":
   print("$20")
 
-<<<<<<< HEAD
 # What to do if the condition does not match 
-=======
-# What to do if the condition does not match
->>>>>>> 11d66b4cbf65015f8e250c86c7851b9bd4fdb5b2
 else:
   print("$100")
