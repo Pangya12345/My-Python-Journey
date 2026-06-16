3d3f0a5d594359c018da0ab5c0349937e29e7b28
Pangya12345 <pholphirulp@gmail.com>
2026-06-16 22:59:25 +0700

    make

--- CS50 - Python/Problem Set/Pset4/figlet.py
+++ CS50 - Python/Problem Set/Pset4/figlet.py
@@ -3,27 +3,36 @@ import random
 import pyfiglet
 
 fronts = pyfiglet.FigletFont.getFonts()
-user_input = input("Input: ")
-
-
-print(sys.argv)
-print(len(sys.argv))
+correct_list = ["-f", "--font"]
+correct_lenght = [1, 3]
 
 if len(sys.argv) == 1:
+    user_input = input("Input: ")
     print(pyfiglet.figlet_format(user_input, font = random.choice(fronts)))
 
 elif len(sys.argv) == 2:
     sys.exit("Invalid usage")
 
 elif len(sys.argv) == 3:
-    if sys.argv[1] != "-f" or sys.argv[1] != "--font":
+    if sys.argv[1] not in correct_list:
         sys.exit("Invalid usage")
 
     elif sys.argv[2] not in fronts:
-        sys.exit("Invalid usage")
+        sys.exit("Invaid usage")
     
-    elif sys.argv[1] == "-f" or sys.argv[1] == "--font" and sys.argv[2] in fronts:
+    else:
+        user_input = input("Input ")
         print(pyfiglet.figlet_format(user_input, font=sys.argv[2]))
+        
+
+elif len(sys.argv) not in correct_lenght:
+    sys.exit("Invalid usage")
+
+
+
+
+    
+
     
 
 
